775ed4916eb8028252db4dae5a0b71d865568638
Dave Pearson <dave.pearson@synpromics.com>
2020-08-25 09:26:49 +0100

    :bookmark: v1.0

--- goto-line-faster.el
+++ goto-line-faster.el
@@ -2,7 +2,7 @@
 ;; Copyright 2020 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 0.1
+;; Version: 1.0
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/goto-line-faster.el
 ;; Package-Requires: ((emacs "24") (bind-key "2.0"))
